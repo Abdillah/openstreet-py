d168f05c463df6c25660967ed8b114a57d2606b3
Hernawan Fa'iz Abdillah <hernawan.faiz.abdillah@gmail.com>
2021-05-17 10:40:07 +0700

    Change versioning

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools_rust import Binding, RustExtension
 
 setup(
     name = "openstreet",
-    version = "0.1",
+    version = "0.0.1",
     rust_extensions = [ RustExtension("openstreet._binding", binding = Binding.PyO3) ],
     packages = [ "openstreet" ],
     # rust extensions are not zip safe, just like C-extensions.
