fc2a3989ae6073f1740e196a0de28e19d7a7af75
Hernawan Fa'iz Abdillah <hernawan.faiz.abdillah@gmail.com>
2021-05-17 11:13:57 +0700

    Add shell.nix deps

--- shell.nix
+++ shell.nix
@@ -6,6 +6,9 @@ pkgs.mkShell {
     pkgs.python38Full
     pkgs.python38Packages.pip
     pkgs.python38Packages.setuptools
+    pkgs.python38Packages.sphinx
+    pkgs.python38Packages.readthedocs-sphinx-ext
+    pkgs.python38Packages.sphinx_rtd_theme
 
     # keep this line if you use bash
     pkgs.bashInteractive
