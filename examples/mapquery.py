1780f203b3c0a9aa16d2819e116282d858b7c43b
Hernawan Fa'iz Abdillah <hernawan.faiz.abdillah@gmail.com>
2021-05-19 10:52:14 +0700

    Refactor example

--- examples/mapquery.py
+++ examples/mapquery.py
@@ -7,6 +7,30 @@ sys.path.insert(0, os.path.abspath(dirname + '/../build/lib'))
 
 from openstreet import Map
 
-map = Map("./resources/madina.osm")
-print(map.nodes().by_id(8623725793))
-print(map.nodes().by_tag_in("highway", [ "primary" ]).get())
+def example_get(map):
+    print(map.nodes().by_id(8623725793))
+
+    ctr = 5
+    for node in map.nodes().where_tag_in("highway", [ "primary" ]).get():
+        if ctr == 0:
+            break;
+
+        print("Node id:", node.id)
+        print("- ", node.tags)
+
+        ctr = ctr - 1
+
+def example_iterator(map):
+    ctr = 5
+    for node in map.nodes().where_tag_in("highway", [ "primary" ]):
+        if ctr == 0:
+            break;
+
+        print("Node id:", node.id)
+
+        ctr = ctr - 1
+
+if __init__ == "__main__":
+    map = Map("./resources/madina.osm")
+    example_get(map)
+    example_iterator(map)
