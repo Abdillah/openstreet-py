6713ed61f6fa0dce79d8533fb93fb01a17b0fe1c
Hernawan Fa'iz Abdillah <hernawan.faiz.abdillah@gmail.com>
2021-06-02 15:13:47 +0700

    Fix setuptools build module without PyInit entrypoint

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools_rust import Binding, RustExtension
 setup(
     name = "openstreet",
     version = "0.0.1",
-    rust_extensions = [ RustExtension("openstreet._binding", binding = Binding.PyO3) ],
+    rust_extensions = [ RustExtension("openstreet._binding", binding = Binding.PyO3, features = ["extension-module"]) ],
     packages = [ "openstreet" ],
     # rust extensions are not zip safe, just like C-extensions.
     zip_safe = False,
